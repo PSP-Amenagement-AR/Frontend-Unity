4a75d638506f8e0f02d2b10002f515cf5bb49f28
Arnaud <arnaud.brown@epitech.eu>
2020-04-16 19:22:54 +0200

    First Menu Display with slide on click

---
+++ Assets/Scripts/AddHandler.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddHandler : MonoBehaviour
+{
+
+    public void AddModel()
+    {
+        Debug.Log("click on button");
+    }
+}
---
+++ Assets/Scripts/MenuHandler.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuHandler : MonoBehaviour
+{
+    public Animator animator;
+    public void OpenMenu()
+    {
+        if (animator != null)
+        {
+            animator.SetBool("open", !animator.GetBool("open"));
+        }
+    }
+}
