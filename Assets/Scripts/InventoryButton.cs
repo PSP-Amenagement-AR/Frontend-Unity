820408d04416fbb023ec13201f278b7d4151dd53
jeanbosc <jean.bosc.ozanam@gmail.com>
2020-04-21 10:32:16 +0200

    Adding Scripts for Item management

---
+++ Assets/Scripts/InventoryButton.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryButton : MonoBehaviour
+{
+    [SerializeField]
+    public Image myIcon;
+
+    [SerializeField]
+    //private Item myItem;
+    private Image myItem;
+
+    public Button myButton;
+
+    void Start()
+    {
+        Button btn = myButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        Debug.Log(myIcon.sprite);
+        //myItem.GetComponent<Item>().SetIcon(myIcon.sprite);
+        myItem.GetComponent<Image>().sprite = myIcon.sprite;
+    }
+
+    public void SetIcon(Sprite mySprite)
+    {
+        myIcon.sprite = mySprite;
+    }
+
+    /*void Update()
+    {
+        Debug.Log("Test");
+        if (Input.GetMouseButton(0))
+        {
+            Debug.Log("Click on button");
+            Debug.Log(myIcon.sprite);
+            Debug.Log("Next Step => Item apparition");
+            //myItem.GetComponent<Item>().SetIcon(myIcon.sprite);
+            myItem.GetComponent<Image>().sprite = myIcon.sprite;
+            Debug.Log("Sprite Done");
+        } else
+        {
+            Debug.Log("chelou");
+        }
+        Debug.Log("Test Final");
+    }*/
+}
