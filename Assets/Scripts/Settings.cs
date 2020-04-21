778ce23f0a97c6cb90f882240ec42b8dd2971eb7
Arnaud <arnaud.brown@epitech.eu>
2020-04-21 16:39:25 +0200

    add Profil context

---
+++ Assets/Scripts/Settings.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Settings : MonoBehaviour
+{
+    void Start()
+    {
+        /* QualitySettings.vSyncCount = 2;*/
+        Application.targetFrameRate = 120;
+    }
+}
