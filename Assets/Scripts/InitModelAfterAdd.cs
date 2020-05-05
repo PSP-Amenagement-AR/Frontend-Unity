c5f04277988c8d2a0c1711fce663c0f04f781db0
Arnaud <arnaud.brown@epitech.eu>
2020-05-05 15:14:35 +0200

    joysticks rotation + movement + add models + delete selected model

---
+++ Assets/Scripts/InitModelAfterAdd.cs
@@ -0,0 +1,38 @@
+﻿using System.CodeDom;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InitModelAfterAdd : MonoBehaviour
+{
+
+    public GameObject Cube;
+    public Text DebugField;
+
+    public void AddModel()
+    {
+        Model[] allModels = FindObjectsOfType<Model>();
+        foreach (var mod in allModels)
+        {
+            ModelBehaviour mb = mod.GetComponent<ModelBehaviour>() as ModelBehaviour;
+            mb.SetSelected(false);
+        }
+
+        GameObject model = Instantiate(Cube, transform.position, transform.rotation);
+        ModelBehaviour modelBehaviour = model.AddComponent<ModelBehaviour>() as ModelBehaviour;
+        modelBehaviour.debugField = DebugField;
+    }
+
+    public void DeleteSelectedModel()
+    {
+        Model[] allModels = FindObjectsOfType<Model>();
+        
+        foreach (Model mod in allModels)
+        {
+            ModelBehaviour mb = mod.GetComponent<ModelBehaviour>() as ModelBehaviour;
+            if (mb.IsSelected())
+                Destroy(mod.gameObject);
+        }
+    }
+}
