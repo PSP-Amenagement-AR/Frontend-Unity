b26e81b16654b30c9385d2f7cd785bfcf69d5dab
Arnaud <44978600+Gerroll@users.noreply.github.com>
2020-05-06 15:51:03 +0200

    Movement of selected obj (#12)
    * static login page + renaming todo
    
    * joysticks rotation + movement + add models + delete selected model
    
    * BaseModel
    
    * bullshit
    
    * joystick + feuille A4 + selecte interface + cleaning code
    
    * up gitignore

--- Assets/Scripts/GlobalStatus.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class GlobalStatus
-{
-    private static bool EtatSelected = false;
-    private static GameObject objSelect;
-}
---
+++ Assets/Scripts/ModelBehaviour.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ModelBehaviour : MonoBehaviour
+{
+
+    private bool Selected = false;
+    private Joystick MovementJoystick;
+    private Joystick RotationJoystick;
+    public Text debugField;
+
+    void Start()
+    {
+        Joystick[] Joysticks;
+        Joysticks = FindObjectsOfType<Joystick>();
+        RotationJoystick = Joysticks[0];
+        MovementJoystick = Joysticks[1];
+
+        Debug.Log(MovementJoystick);
+        Debug.Log(RotationJoystick);
+        Selected = true;
+    }
+
+    private void Update()
+    {
+        if (!Selected)
+            return;
+
+        var rigidbody = GetComponent<Rigidbody>();
+        rigidbody.velocity = new Vector3(MovementJoystick.Horizontal * 50f,
+            rigidbody.velocity.y,
+            MovementJoystick.Vertical * 50f);
+        float speed = RotationJoystick.Horizontal;
+        transform.Rotate(Vector3.down * speed * 50f * Time.deltaTime);
+    }
+
+    public void SetSelected(bool val)
+    {
+        Selected = val;
+    }
+    public bool IsSelected()
+    {
+        return Selected;
+    }
+}
