e40a63b44c94f88ae8fd567d87902407dd9cd5be
jeanbosc <jean.bosc.ozanam@gmail.com>
2020-05-10 12:26:33 +0200

    list stages correction

--- Assets/Scripts/InventoryControl.cs
+++ Assets/Scripts/InventoryControl.cs
@@ -13,18 +13,37 @@ public class InventoryControl : MonoBehaviour
     [SerializeField]
     private GridLayoutGroup gridGroup;
 
+    public bool isItemList;
+
     void Start()
     {
         buttonTemplate.SetActive(false);
         playerInventory = new List<PlayerItem>();
+
+        if (isItemList)
+            UpdateListItems();
+        else
+            UpdateListStages();
+
+        GenInventory();
+    }
+
+    void UpdateListItems()
+    {
         AddNewItem("chair_1", "Sprites/Items/Chair");
         AddNewItem("bed_1", "Sprites/Items/Dark White Bed");
         AddNewItem("table_1", "Sprites/Items/Desk");
         AddNewItem("bed_2", "Sprites/Items/Grey Bed");
         AddNewItem("kitchen_chair_1", "Sprites/Items/Kitchen Chair");
         AddNewItem("torchere_1", "Sprites/Items/Lamp");
+    }
 
-        GenInventory();
+    void UpdateListStages()
+    {
+        AddNewItem("hall", "Sprites/Stages/hall");
+        AddNewItem("kitchen", "Sprites/Stages/kitchen");
+        AddNewItem("parent_room", "Sprites/Stages/parent_room");
+        AddNewItem("room", "Sprites/Stages/room");
     }
 
     void AddNewItem(string name, string spritePath)
@@ -57,7 +76,6 @@ public class InventoryControl : MonoBehaviour
             newButton.GetComponent<InventoryButton>().SetName(newItem.iconName);
             newButton.transform.SetParent(buttonTemplate.transform.parent, false);
         }
-
     }
 
     public class PlayerItem
