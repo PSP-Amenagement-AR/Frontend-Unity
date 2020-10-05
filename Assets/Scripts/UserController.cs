edff93ff814262a185d92601b99f4e53b9b44309
jeanbosc <jean.bosc.ozanam@gmail.com>
2020-10-05 16:19:50 +0200

    feature/ popup label with warnings and register validation

--- Assets/Scripts/UserController.cs
+++ Assets/Scripts/UserController.cs
@@ -5,48 +5,27 @@ using UnityEngine.UI;
 
 public class UserController : MonoBehaviour
 {
+    public GameObject popup;
+    public Text popupText;
+    public ScreenHandler canvas;
+
     public Button submitButton;
     public InputField firstnameField;
     public InputField lastnameField;
     public InputField mailField;
     public InputField passwordField;
     public InputField confirmedPasswordField;
+
     private bool created = false;
 
     private void Awake()
     {
         submitButton.onClick.AddListener(() =>
         {
-            if ((firstnameField.text.ToString() != "") && (lastnameField.text.ToString() != "") &&
-                (mailField.text.ToString() != "") && (passwordField.text.ToString() != "") && (confirmedPasswordField.text.ToString() != ""))
+            if (UserCheckInformations())
             {
-                if (passwordField.text.ToString() != confirmedPasswordField.text.ToString())
-                {
-                    Debug.Log("Not the same password");
-                    return;
-                }
-                else if (passwordField.text.ToString().Length < 9)
-                {
-                    Debug.Log("Password too short");
-                }
-                else if ((mailField.text.ToString().Contains("@") == false) || (mailField.text.ToString().Contains(".com") == false))
-                {
-                    Debug.Log("The mail is not correct");
-                }
-                else
-                {
-                    Debug.Log("User created " + firstnameField.text.ToString() + " "
-                        + lastnameField.text.ToString() + " "
-                        + mailField.text.ToString() + " "
-                        + passwordField.text.ToString() + " "
-                        + confirmedPasswordField.text.ToString());
-
-                    created = true;
-                }
-            } else
-            {
-                Debug.Log("One or many field(s) are missing");
-                return;
+                canvas.CloseRegistration();
+                canvas.OpenLogin();
             }
         });
     }
@@ -58,6 +37,54 @@ public class UserController : MonoBehaviour
 
     void Update()
     {
+        
+    }
 
+    bool UserCheckInformations()
+    {
+        if ((firstnameField.text.ToString() != "") && (lastnameField.text.ToString() != "") &&
+                (mailField.text.ToString() != "") && (passwordField.text.ToString() != "") && (confirmedPasswordField.text.ToString() != ""))
+        {
+            if ((mailField.text.ToString().Contains("@") == false) || (mailField.text.ToString().Contains(".com") == false))
+            {
+                InitPopup("The mail is not correct");
+                created = false;
+            }
+            else if(passwordField.text.ToString() != confirmedPasswordField.text.ToString())
+            {
+                InitPopup("Not the same password");
+                created = false;
+            }
+            else if (passwordField.text.ToString().Length < 9)
+            {
+                InitPopup("Password too short");
+                created = false;
+            }
+            else
+            {
+                InitPopup("User created");
+
+                created = true;
+            }
+        }
+        else
+        {
+            InitPopup("One or many field(s) are missing");
+            created = false;
+        }
+        return created;
+    }
+
+    void InitPopup(string str)
+    {
+        popupText.text = str;
+        StartCoroutine(PopupText());
+    }
+
+    IEnumerator PopupText()
+    {
+        popup.SetActive(true);
+        yield return new WaitForSeconds(2);
+        popup.SetActive(false);
     }
 }
