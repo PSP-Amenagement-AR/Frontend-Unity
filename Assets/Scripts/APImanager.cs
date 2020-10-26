3301bc2adc70292b7c3f74cf754c09873a497d61
jeanbosc <jean.bosc.ozanam@gmail.com>
2020-10-26 09:26:42 +0100

    init API manager and request to test API

---
+++ Assets/Scripts/APImanager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using SimpleJSON;
+
+public class APImanager : MonoBehaviour
+{
+    public string jsonURL;
+
+    void Start()
+    {
+        StartCoroutine(GetRequest());
+    }
+
+    IEnumerator GetRequest()
+    {
+        Debug.Log("Processing Data");
+        /* WWW _www = new WWW(jsonURL);
+         yield return _www;
+
+         if (_www.error == null)
+         {
+             processJsonData(_www.text);
+         }
+         else
+         {
+             Debug.Log("Error something went wrong");
+         }*/
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(jsonURL))
+        {
+            yield return webRequest.SendWebRequest();
+
+            var N = JSON.Parse(webRequest.downloadHandler.text);
+            /*string result = N["balls"][0]["name"].Value;
+            Debug.Log(result);*/
+            processJsonData(webRequest.downloadHandler.text);
+        }
+    }
+
+    private void processJsonData(string data)
+    {
+        jsonDataClass jsnData = JsonUtility.FromJson<jsonDataClass>(data);
+
+        foreach (ballList x in jsnData.balls)
+        {
+            Debug.Log(x.name + ": " + x.description);
+        }
+    }
+}
---
+++ Assets/Scripts/jsonDataClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class jsonDataClass
+{
+    public string playerName;
+    public List<ballList> balls;
+    public List<loadinging> loadingImages;
+}
+
+[Serializable]
+public class ballList
+{
+    public string name;
+    public string description;
+    public int price;
+    public string image;
+    public int size;
+    public string weight;
+    public string free;
+}
+
+[Serializable]
+public class loadinging
+{
+    public string image;
+}
